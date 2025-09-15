664e55d40964a90b3cc403b0ef26be526759054a
Saptya001 <saptyadeepsomlaptop1@gmail.com>
2025-09-15 19:41:33 +0530

    15.09.2025
    dispatched

--- my_app/src/App.tsx
+++ my_app/src/App.tsx
@@ -7,15 +7,13 @@ function App() {
   const [count, setCount] = useState<number>(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <div>
+      <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,7 +26,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
